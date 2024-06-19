830a93c1138852bcfd9c5a03250cf8c48d121bb9
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-06-18 22:12:53 -0300

    changes env

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const port = process.env.PORT;
 const {getTeamsHandler} = require("./src/handlers/teamsHandlers.js")
 
 conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${port}`);
   
 })
 }).catch(error => console.error(error));
